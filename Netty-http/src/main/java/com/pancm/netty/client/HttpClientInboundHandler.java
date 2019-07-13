c2fcb29bdd46a9bb03fb29a42eac0d90eefde2ef
huangyan <huangyan@huangyan-PC>
2019-07-13 12:44:44 +0800

    提交

---
+++ Netty-http/src/main/java/com/pancm/netty/client/HttpClientInboundHandler.java
@@ -0,0 +1,33 @@
+package com.pancm.netty.client;
+ 
+import java.nio.charset.Charset;
+
+import com.alibaba.fastjson.JSON;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+ 
+public class HttpClientInboundHandler extends ChannelInboundHandlerAdapter {
+ 
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpResponse) 
+        {
+            HttpResponse response = (HttpResponse) msg;
+            System.out.println("CONTENT_TYPE:" + response.headers().get(HttpHeaders.Names.CONTENT_TYPE));
+           // System.out.println(JSON.toJSON(response));
+            
+        }
+        if(msg instanceof HttpContent)
+        {
+            HttpContent content = (HttpContent)msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(Charset.forName("GBK")));
+            buf.release();
+        }
+    }
+}
